0c065e09ac48597ad065a628568e12f7704905a2
Sherwin688 <sherwin.dcosta688@gmail.com>
2023-09-01 21:05:37 +0530

    mobile responsive

--- js/script.js
+++ js/script.js
@@ -27,110 +27,116 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-
-window.addEventListener( 'load', () => {
-	const COMPONENT_SELECTOR = '.carousel__wrapper';
-	const CONTROLS_SELECTOR = '.carousel__controls';
-  const CONTENT_SELECTOR = '.carousel__content';
-
-	const components = document.querySelectorAll( COMPONENT_SELECTOR );
-
-	for ( let i = 0; i < components.length; i++ ) {
-		const component = components[ i ];
-		const content = component.querySelector( CONTENT_SELECTOR );
-		let x = 0;
-		let mx = 0;
-		const maxScrollWidth = content.scrollWidth - content.clientWidth / 2 - content.clientWidth / 2;
-		const nextButton = component.querySelector( '.arrow-next' );
-		const prevButton = component.querySelector( '.arrow-prev' );
-
-		if ( maxScrollWidth !== 0 ) {
-			component.classList.add( 'has-arrows' );
-		}
-
-		if ( nextButton ) {
-			nextButton.addEventListener( 'click', function ( event ) {
-				event.preventDefault();
-				x = content.clientWidth / 2 + content.scrollLeft + 0;
-				content.scroll( {
-					left: x,
-					behavior: 'smooth',
-				} );
-			} );
-		}
-
-		if ( prevButton ) {
-			prevButton.addEventListener( 'click', function ( event ) {
-				event.preventDefault();
-				x = content.clientWidth / 2 - content.scrollLeft + 0;
-				content.scroll( {
-					left: -x,
-					behavior: 'smooth',
-				} );
-			} );
-		}
-
-		/**
-		 * Mouse move handler.
-		 *
-		 * @param {object} e event object.
-		 */
-		const mousemoveHandler = ( e ) => {
-			const mx2 = e.pageX - content.offsetLeft;
-			if ( mx ) {
-				content.scrollLeft = content.sx + mx - mx2;
+function setCarousel(){
+	{
+		const COMPONENT_SELECTOR = '.carousel__wrapper';
+		const CONTROLS_SELECTOR = '.carousel__controls';
+	  const CONTENT_SELECTOR = '.carousel__content';
+	
+		const components = document.querySelectorAll( COMPONENT_SELECTOR );
+	
+		for ( let i = 0; i < components.length; i++ ) {
+			const component = components[ i ];
+			const content = component.querySelector( CONTENT_SELECTOR );
+			let x = 0;
+			let mx = 0;
+			const maxScrollWidth = content.scrollWidth - content.clientWidth / 2 - content.clientWidth / 2;
+			const nextButton = component.querySelector( '.arrow-next' );
+			const prevButton = component.querySelector( '.arrow-prev' );
+	
+			if ( maxScrollWidth !== 0 ) {
+				component.classList.add( 'has-arrows' );
 			}
-		};
-
-		/**
-		 * Mouse down handler.
-		 *
-		 * @param {object} e event object.
-		 */
-		const mousedownHandler = ( e ) => {
-			content.sx = content.scrollLeft;
-			mx = e.pageX - content.offsetLeft;
-			content.classList.add( 'dragging' );
-		};
-
-		/**
-		 * Scroll handler.
-		 */
-		const scrollHandler = () => {
-			toggleArrows();
-		};
-
-		/**
-		 * Toggle arrow handler.
-		 */
-		const toggleArrows = () => {
-			if ( content.scrollLeft > maxScrollWidth - 10 ) {
-				nextButton.classList.add( 'disabled' );
-			} else if ( content.scrollLeft < 10 ) {
-				prevButton.classList.add( 'disabled' );
-			} else {
-				nextButton.classList.remove( 'disabled' );
-				prevButton.classList.remove( 'disabled' );
+	
+			if ( nextButton ) {
+				nextButton.addEventListener( 'click', function ( event ) {
+					event.preventDefault();
+					x = content.clientWidth / 2 + content.scrollLeft + 0;
+					content.scroll( {
+						left: x,
+						behavior: 'smooth',
+					} );
+				} );
 			}
-		};
-
-		/**
-		 * Mouse up handler.
-		 */
-		const mouseupHandler = () => {
-			mx = 0;
-			content.classList.remove( 'dragging' );
-		};
-
-		content.addEventListener( 'mousemove', mousemoveHandler );
-		content.addEventListener( 'mousedown', mousedownHandler );
-		if ( component.querySelector( CONTROLS_SELECTOR ) !== undefined ) {
-			content.addEventListener( 'scroll', scrollHandler );
+	
+			if ( prevButton ) {
+				prevButton.addEventListener( 'click', function ( event ) {
+					event.preventDefault();
+					x = content.clientWidth / 2 - content.scrollLeft + 0;
+					content.scroll( {
+						left: -x,
+						behavior: 'smooth',
+					} );
+				} );
+			}
+	
+			/**
+			 * Mouse move handler.
+			 *
+			 * @param {object} e event object.
+			 */
+			const mousemoveHandler = ( e ) => {
+				const mx2 = e.pageX - content.offsetLeft;
+				if ( mx ) {
+					content.scrollLeft = content.sx + mx - mx2;
+				}
+			};
+	
+			/**
+			 * Mouse down handler.
+			 *
+			 * @param {object} e event object.
+			 */
+			const mousedownHandler = ( e ) => {
+				content.sx = content.scrollLeft;
+				mx = e.pageX - content.offsetLeft;
+				content.classList.add( 'dragging' );
+			};
+	
+			/**
+			 * Scroll handler.
+			 */
+			const scrollHandler = () => {
+				toggleArrows();
+			};
+	
+			/**
+			 * Toggle arrow handler.
+			 */
+			const toggleArrows = () => {
+				if ( content.scrollLeft > maxScrollWidth - 10 ) {
+					nextButton.classList.add( 'disabled' );
+				} else if ( content.scrollLeft < 10 ) {
+					prevButton.classList.add( 'disabled' );
+				} else {
+					nextButton.classList.remove( 'disabled' );
+					prevButton.classList.remove( 'disabled' );
+				}
+			};
+	
+			/**
+			 * Mouse up handler.
+			 */
+			const mouseupHandler = () => {
+				mx = 0;
+				content.classList.remove( 'dragging' );
+			};
+	
+			content.addEventListener( 'mousemove', mousemoveHandler );
+			content.addEventListener( 'mousedown', mousedownHandler );
+			if ( component.querySelector( CONTROLS_SELECTOR ) !== undefined ) {
+				content.addEventListener( 'scroll', scrollHandler );
+			}
+			content.addEventListener( 'mouseup', mouseupHandler );
+			content.addEventListener( 'mouseleave', mouseupHandler );
 		}
-		content.addEventListener( 'mouseup', mouseupHandler );
-		content.addEventListener( 'mouseleave', mouseupHandler );
 	}
-} );
+}
+window.addEventListener("resize", function(){
+	setCarousel();
+  });
+
+window.addEventListener( 'load', () => setCarousel() );
 
 function highlightToday() {
 	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -174,3 +180,43 @@ function handleFacebook(){
 	console.log(instaOpen);
 
 }
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+const yearDate = new Date();
+const currentYear = yearDate.getFullYear();
+
+document.getElementById("year").innerText = currentYear;
+
+
+
+
+
+
+function myFunction(x) {
+	var mobileNavs = document.getElementById("mobile-navs");
+	x.classList.toggle("change");
+
+	if(mobileNavs.style.display==="block"){
+		mobileNavs.style.display="none"
+	}
+	else{
+		mobileNavs.style.display="block"
+
+	}
+	
+  }
